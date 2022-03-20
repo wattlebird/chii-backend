ceac1d1c98c2075be92586ae17a55a932ae8720b
Ronnie Wang <geniusxiaoguai@gmail.com>
2022-03-20 11:00:25 +0800

    feat: azure function for db sync

--- chiiv2/Database.cs
+++ chiiv2/Database.cs
@@ -4,7 +4,12 @@ using System.Linq;
 using System.IO;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
+using Azure.Storage.Blobs;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System.Reflection;
+using System.Runtime.Intrinsics.X86;
 
 namespace chiiv2
 {
@@ -16,22 +21,69 @@ namespace chiiv2
             _context = context;
         }
 
-        [FunctionName("syncrank")]
+        [FunctionName("sync")]
         [StorageAccount("IkelyConnectionString")]
-        public void SyncRank([BlobTrigger("database/{name}")]Stream stream, string name, ILogger log)
+        public void Sync([BlobTrigger("database/{name}")] Stream stream, [Blob("database")] BlobContainerClient containerClient, string name, ILogger log)
+        {
+            if (name.StartsWith("customrank") && name.EndsWith("csv"))
+            {
+                log.LogInformation($"Function Sync processing file {name}...");
+                name = name.Split('.')[0];
+                string[] vs = name.Split('_');
+                DateTime date = new DateTime(Int32.Parse(vs[1]), Int32.Parse(vs[2]), Int32.Parse(vs[3]));
+                if (_context.Timestamps.Count() > 0 && _context.Timestamps.First().Date > date)
+                {
+                    log.LogInformation($"Data on {date.ToString()} already exists, exit.");
+                    return;
+                }
+
+                // Removing existing data in db
+                int count = _context.CustomRanks.Count();
+                if (count > 0)
+                {
+                    _context.CustomRanks.RemoveRange(_context.CustomRanks);
+                    _context.SaveChanges();
+                }
+                count = _context.Tags.Count();
+                if (count > 0)
+                {
+                    _context.Tags.RemoveRange(_context.Tags);
+                    _context.SaveChanges();
+                }
+                count = _context.SubjectEntities.Count();
+                if (count > 0)
+                {
+                    _context.SubjectEntities.RemoveRange(_context.SubjectEntities);
+                    _context.SaveChanges();
+                }
+                count = _context.Subjects.Count();
+                if (count > 0)
+                {
+                    _context.Subjects.RemoveRange(_context.Subjects);
+                    _context.SaveChanges();
+                }
+
+                BlobClient blobClient = containerClient.GetBlobClient($"subject_archive_{vs[1]}_{vs[2]}_{vs[3]}.jsonlines");
+                SyncSubject(blobClient.OpenRead(), blobClient.Name, log);
+                blobClient = containerClient.GetBlobClient($"subject_entity_{vs[1]}_{vs[2]}_{vs[3]}.jsonlines");
+                SyncSubjectEntity(blobClient.OpenRead(), blobClient.Name, log);
+                blobClient = containerClient.GetBlobClient($"tags_{vs[1]}_{vs[2]}_{vs[3]}.jsonlines");
+                SyncTags(blobClient.OpenRead(), blobClient.Name, log);
+                blobClient = containerClient.GetBlobClient($"customrank_{vs[1]}_{vs[2]}_{vs[3]}.csv");
+                SyncRank(blobClient.OpenRead(), blobClient.Name, log);
+
+                _context.Timestamps.RemoveRange(_context.Timestamps);
+                _context.Timestamps.Add(new Timestamp { Date = date });
+                _context.SaveChanges();
+                log.LogInformation($"Timestamp updated.");
+            }
+        }
+
+        private void SyncRank(Stream stream, string name, ILogger log)
         {
             if (name.StartsWith("customrank") && name.EndsWith("csv"))
             {
                 log.LogInformation($"Function SyncRank processing file {name}...");
-                name = name.Split('.')[0];
-                string[] vs = name.Split('_');
-                DateTime date = new DateTime(Int32.Parse(vs[1]), Int32.Parse(vs[2]), Int32.Parse(vs[3]));
-                Timestamp ts = _context.Timestamps.First();
-                if (ts.Date == date)
-                {
-                    log.LogInformation($"Customrank on {date.ToString()} already exists, exit.");
-                    return;
-                }
 
                 List<CustomRank> ranks = new List<CustomRank>();
                 using (var reader = new StreamReader(stream))
@@ -51,21 +103,78 @@ namespace chiiv2
                 }
                 log.LogInformation($"Read {ranks.Count()} customrank records.");
 
-                int count = _context.CustomRanks.Count();
-                if (count > 0)
-                {
-                    _context.CustomRanks.RemoveRange(_context.CustomRanks);
-                    _context.SaveChanges();
-                }
                 _context.CustomRanks.AddRange(ranks);
                 _context.SaveChanges();
                 log.LogInformation($"Customrank updated.");
+            }
+        }
 
+        private void SyncSubject(Stream stream, string name, ILogger log)
+        {
+            if (name.StartsWith("subject_archive") && name.EndsWith("jsonlines"))
+            {
+                log.LogInformation($"Function SyncSubject processing file {name}...");
 
-                _context.Timestamps.RemoveRange(_context.Timestamps);
-                _context.Timestamps.Add(new Timestamp { Date = date });
-                _context.SaveChanges();
-                log.LogInformation($"Timestamp updated.");
+                using (var reader = new StreamReader(stream))
+                {
+                    List<Subject> subjects = new List<Subject>();
+                    while (reader.Peek() >= 0)
+                    {
+                        string record = reader.ReadLine().Trim();
+                        Subject subject = JsonConvert.DeserializeObject<Subject>(record);
+                        subjects.Add(subject);
+                    }
+
+                    _context.Subjects.AddRange(subjects);
+                    _context.SaveChanges();
+                }
+                log.LogInformation($"{_context.Subjects.Count()} subject records saved.");
+            }
+        }
+
+        private void SyncSubjectEntity(Stream stream, string name, ILogger log)
+        {
+            if (name.StartsWith("subject_entity") && name.EndsWith("jsonlines"))
+            {
+                log.LogInformation($"Function SyncSubjectEntity processing file {name}...");
+
+                using (var reader = new StreamReader(stream))
+                {
+                    List<SubjectEntity> subjectEntities = new List<SubjectEntity>();
+                    while (reader.Peek() >= 0)
+                    {
+                        string record = reader.ReadLine().Trim();
+                        SubjectEntity subjectEntity = JsonConvert.DeserializeObject<SubjectEntity>(record);
+                        subjectEntities.Add(subjectEntity);
+                    }
+
+                    _context.SubjectEntities.AddRange(subjectEntities);
+                    _context.SaveChanges();
+                }
+                log.LogInformation($"{_context.SubjectEntities.Count()} subject entity records saved.");
+            }
+        }
+
+        private void SyncTags(Stream stream, string name, ILogger log)
+        {
+            if (name.StartsWith("tags") && name.EndsWith("jsonlines"))
+            {
+                log.LogInformation($"Function SyncTags processing file {name}...");
+
+                using (var reader = new StreamReader(stream))
+                {
+                    List<Tag> tags = new List<Tag>();
+                    while (reader.Peek() >= 0)
+                    {
+                        string record = reader.ReadLine().Trim();
+                        Tag tag = JsonConvert.DeserializeObject<Tag>(record);
+                        tags.Add(tag);
+                    }
+
+                    _context.Tags.AddRange(tags);
+                    _context.SaveChanges();
+                }
+                log.LogInformation($"{_context.Tags.Count()} tag records saved.");
             }
         }
     }
--- chiiv2/Model.cs
+++ chiiv2/Model.cs
@@ -3,28 +3,46 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 
 namespace chiiv2
 {
+    [JsonObject(MemberSerialization.OptIn)]
     public class Subject
     {
         [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [JsonProperty(PropertyName = "id")]
         public int Id { get; set; }
         [Required]
+        [JsonProperty(PropertyName = "name")]
         public string Name { get; set; }
+        [JsonProperty(PropertyName = "name_cn")]
         public string? NameCN { get; set; }
+        [JsonProperty(PropertyName = "infobox")]
         public string? Infobox { get; set; }
+        [JsonProperty(PropertyName = "platform")]
         public int Platform { get; set; }
+        [JsonProperty(PropertyName = "summary")]
         public string? Summary { get; set; }
+        [JsonProperty(PropertyName = "rank")]
         public int? Rank { get; set; }
+        [JsonProperty(PropertyName = "nsfw")]
         public bool NSFW { get; set; }
+        [JsonProperty(PropertyName = "type")]
         public string? Type { get; set; }
+        [JsonProperty(PropertyName = "fav_count")]
         public int FavCount { get; set; }
+        [JsonProperty(PropertyName = "rate_count")]
         public int RateCount { get; set; }
+        [JsonProperty(PropertyName = "collect_count")]
         public int CollectCount { get; set; }
+        [JsonProperty(PropertyName = "do_count")]
         public int DoCount { get; set; }
+        [JsonProperty(PropertyName = "dropped_count")]
         public int DroppedCount { get; set; }
+        [JsonProperty(PropertyName = "on_hold_count")]
         public int OnHoldCount { get; set; }
+        [JsonProperty(PropertyName = "wish_count")]
         public int WishCount { get; set; }
 
         public List<Tag> Tags;
@@ -33,26 +51,36 @@ namespace chiiv2
     }
 
     [Index(nameof(SubjectId), nameof(NormalizedContent), IsUnique = true)]
+    [JsonObject(MemberSerialization.OptIn)]
     public class Tag
     {
         [Key]
         public int Id { get; set; }
+        [JsonProperty(PropertyName = "iid")]
         public int SubjectId { get; set; }
+        [JsonProperty(PropertyName = "tags")]
         public string Content { get; set; }
+        [JsonProperty(PropertyName = "tags_normalized")]
         public string NormalizedContent { get; set; }
+        [JsonProperty(PropertyName = "cnt")]
         public int UserCount { get; set; }
+        [JsonProperty(PropertyName = "confidence")]
         public double Confidence { get; set; }
 
         public Subject Subject;
     }
 
     [Index(nameof(NormalizedAlias))]
+    [JsonObject(MemberSerialization.OptIn)]
     public class SubjectEntity
     {
         [Key]
         public int Id { get; set; }
+        [JsonProperty(PropertyName = "id")]
         public int SubjectId { get; set; }
+        [JsonProperty(PropertyName = "alias")]
         public string Alias { get; set; }
+        [JsonProperty(PropertyName = "alias_normalized")]
         public string NormalizedAlias { get; set; }
 
         public Subject Subject;
